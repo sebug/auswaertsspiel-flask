de9fc602a94552d6781c65623411f35c76e1e79c
sebug <sebastian.gfeller@gmail.com>
2020-10-08 15:50:25 +0200

    Adding with rendering of a template

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Ohai from CI overridden trigger, with an update."
+
+@app.route("/players")
+def players():
+    return render_template('players.html')
