ec8cea1ae7a3fa078ec6654a662d3e779046d76f
sebug <sebastian.gfeller@gmail.com>
2020-10-08 17:09:59 +0200

    Making request to other API

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os.path
 import os
+import base64
+import requests
 
 from flask import Flask, render_template, request, send_from_directory
 app = Flask(__name__)
@@ -19,6 +21,18 @@ def envo():
 def players():
     player_list = ['Marco Wölfli', 'Saidy Janko']
     return render_template('players.html', player_list = player_list)
+
+@app.route("/otherapi")
+def otherapi():
+    access_token_url = 'https://api.srgssr.ch/oauth/v1/accesstoken?grant_type=client_credentials'
+    client_id = os.environ.get('SRG_CONSUMER_KEY')
+    client_secret = os.environ.get('SRG_CONSUMER_SECRET')
+    cs_bytes = (client_id + ':' + client_secret).encode('ascii')
+    cs_encoded = base64.b64encode(cs_bytes)
+    headers = { 'Authorization': 'Basic ' + cs_encoded.decode('ascii') }
+    r = requests.post(access_token_url, headers = headers)
+    return r.json()['organization_name']
+    
 
 @app.route("/js/<path:path>")
 def send_js(path):
